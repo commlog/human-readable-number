994ef6628d59e9136dc2e8d733513d3030686c80
Andrey Pechenev <andrey@pechenev.ru>
2023-01-25 16:02:02 +0300

    feat: Complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function toReadable (number) {
+    var numberList = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    var tensList = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    var result = "";
   
+    if (number < 0) {
+        return "negative " + toReadable(-number);
+    }
+    if (number < 20) {
+        return numberList[number];
+    }
+    if (number < 100) {
+        return tensList[Math.floor(number / 10)] + (number % 10 ? " " + toReadable(number % 10) : "");
+    }
+    if (number < 1000) {
+        return numberList[Math.floor(number / 100)] + " hundred" + (number % 100 ? " " + toReadable(number % 100) : "");
+    }
+    if (number < 1000000) {
+        return toReadable(Math.floor(number / 1000)) + " thousand" + (number % 1000 ? " " + toReadable(number % 1000) : "");
+    }
+    if (number < 1000000000) {
+        return toReadable(Math.floor(number / 1000000)) + " million" + (number % 1000000 ? " " + toReadable(number % 1000000) : "");
+    }
+    return "number too large";
 }
